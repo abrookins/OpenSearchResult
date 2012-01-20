8c17b5ac36f7c1dd8f789b0b96b6ab8da4e5b4ef
Andrew Brookins <a.m.brookins@gmail.com>
2012-01-20 06:23:21 -0800

    First commit

---
+++ README.md
@@ -0,0 +1,24 @@
+# Go to File at Path Plugin for Sublime Text 2
+
+This plugin creates a command that allows you to open files listed in the search
+results of the 'Find in Files' command.
+
+As you move your cursor through the search results, any paths that are openable
+will be highlighted in, by default, the "function" color of your color scheme.
+
+If you run the command while the cursor is on a line that is openable, the file
+will open in a new tab.
+
+## Key Binding
+
+The default key binding is a Vintage command mode key: "go".
+
+## Customizing
+
+You can change various things about the plugin by adding user settings:
+
+- 'highlight_file_scope': The scope that will be used to color the outline for
+openable paths. See your theme file for examples.
+- 'highlight_file_icon': If you want an icon to show up in the gutter next to
+openable paths, include a valid icon name as a string (e.g., 'circle', 'dot')
+(see Sublime Text 2 API docs for View.add_regions() for valid icon names)
---
+++ goto_file_at_path.py
@@ -0,0 +1,59 @@
+import os
+import sublime, sublime_plugin
+
+
+class HighlightFilePaths(sublime_plugin.EventListener):
+    HIGHLIGHT_REGION_NAME = 'HighlightFilePaths'
+    SCOPE_SETTINGS_KEY = 'highlight_file_scope'
+    ICON_SETTINGS_KEY = 'highlight_file_icon'
+    DEFAULT_SCOPE = 'highlight_file_path'
+    DEFAULT_ICON = ''
+
+    def show_highlight(self, view):
+        valid_regions = []
+        scope = view.settings().get(self.SCOPE_SETTINGS_KEY, self.DEFAULT_SCOPE)
+        icon = view.settings().get(self.ICON_SETTINGS_KEY, self.DEFAULT_ICON)
+
+        for s in view.sel():
+            line = view.line(s)
+            line_str = view.substr(view.line(s))
+            if not line_str.startswith('/') or not line_str.endswith(':'):
+                continue
+            valid_regions.append(line)
+
+        if valid_regions:
+            view.add_regions(
+                self.HIGHLIGHT_REGION_NAME, valid_regions, 
+                scope, icon, sublime.DRAW_EMPTY | sublime.DRAW_OUTLINED)
+        else:
+            view.erase_regions(self.HIGHLIGHT_REGION_NAME)
+
+    def on_selection_modified(self, view):
+        if view.settings().get('is_widget') \
+            or not view.settings().get('command_mode'):
+            view.erase_regions(self.HIGHLIGHT_REGION_NAME)
+            return
+        self.show_highlight(view)
+
+    def on_deactivated(self, view):
+        view.erase_regions(self.HIGHLIGHT_REGION_NAME)
+
+    def on_activated(self, view):
+        self.show_highlight(view)
+
+
+class GotoFileAtPathCommand(sublime_plugin.TextCommand):
+    """ Open files listed in the Find in File results buffer. """
+
+    def run(self, edit):
+        """
+        If the user runs this command while on a line that starts with '/' and
+        ends with ':', open the file.
+        """
+        cursor = self.view.sel()[0]
+        line = self.view.substr(self.view.line(cursor))
+
+        if line.startswith('/') and line.endswith(':'):
+            file_path = line.split(':')[0]
+            if os.path.exists(file_path):
+                self.view.window().open_file(file_path)
